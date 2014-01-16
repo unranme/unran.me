32f00c47ea6cb1cb44d0da4e3c249c9810154016
unran.me <unran.me@gmail.com>
2014-01-16 20:16:02 +0200

    design

--- script/main.js
+++ script/main.js
@@ -4,8 +4,16 @@
   var input = document.getElementById('ac');
   var resultsEl = document.getElementById('results');
   var instructionsEl = document.getElementById('instructions');
+  var allokEl = document.getElementById('allok');
+
+  allokEl.style.display = 'none';
+  instructionsEl.style.display = 'none';
 
   input.addEventListener('input', function oninput(e) {
+    allokEl.style.display = 'none';
+    instructionsEl.style.display = 'none';
+
+
     var value =  e.target.value;
     var match = false;
     clearResults();
@@ -20,10 +28,13 @@
       }
 
       if (match) {
-        instructionsEl.style.display = 'none';
+        allokEl.style.display = 'none';
+      } else {
+        allokEl.style.display = 'block';
       }
     } else {
       instructionsEl.style.display = 'block';
+      allokEl.style.display = 'none';
     }
   });
 
@@ -33,7 +44,17 @@
 
   function addResult(item) {
     var li = document.createElement('li');
-    li.textContent = item;
+
+    var icon = document.createElement('span');
+    icon.textContent = '⊘';
+    icon.className = 'icon';
+    li.appendChild(icon);
+
+    var label = document.createElement('span');
+    label.textContent = '⊘';
+    label.textContent = item;
+    li.appendChild(label);
+
     resultsEl.appendChild(li);
   }
 
